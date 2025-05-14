c4f89e0aa861683004933f4fcfd9e0052fe85bf5
Barrister <charlesawuku2010@gmail.com>
2025-05-14 14:32:58 -0500

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,15 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <script src="https://js.paystack.co/v1/inline.js"></script>
-      <Head />
+      <Head>
+        <Script
+          src="https://js.paystack.co/v1/inline.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
